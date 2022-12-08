87f0dea0be75e24096bc50f76673fc08b836d348
Nan Natalia <nan_natalia@yahoo.ro>
2022-12-08 09:49:48 +0200

    first page from second chapter is updated

--- src/chapter1/Page1.java
+++ src/chapter1/Page1.java
@@ -4,5 +4,6 @@ public class Page1 {
     public static void main(String[] args) {
         System.out.println("This is the new book ");
         System.out.println("Hope you will enjoy it");
+        //blablbalablab
     }
 }
