4e39616eeacc032bfa00251c236a03961563f4d9
Nan Natalia <nan_natalia@yahoo.ro>
2022-12-08 09:41:33 +0200

    first page commited

---
+++ src/chapter1/Page1.java
@@ -0,0 +1,8 @@
+package chapter1;
+
+public class Page1 {
+    public static void main(String[] args) {
+        System.out.println("This is the new book ");
+        System.out.println("Hope you will enjoy it");
+    }
+}
